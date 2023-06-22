670f171939cc461a0bf516182d4033ea7c59d4c8
lineitv <134327909+lineitv@users.noreply.github.com>
2023-06-22 20:55:08 +0300

    Add files via upload

---
+++ src/type/type.ts
@@ -0,0 +1,11 @@
+export type AuthorContent = {
+    readonly slug: string;
+    readonly name: string;
+    readonly introduction: string;
+  };
+
+  export type TagContent = {
+    readonly slug: string;
+    readonly name: string;
+  };
+  
