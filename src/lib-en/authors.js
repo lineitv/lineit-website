c1c59d5f6417f652a56928381e2ae91e13a55f41
lineitv <134327909+lineitv@users.noreply.github.com>
2023-06-22 20:56:21 +0300

    Update and rename authors.ts to authors.js

---
+++ src/lib-en/authors.js
@@ -0,0 +1,15 @@
+import authors from "../../meta-en/authors.yml";
+
+const authorMap = generateAuthorMap();
+
+function generateAuthorMap() {
+  let result = {};
+  for (const author of authors.authors) {
+    result[author.slug] = author;
+  }
+  return result;
+}
+
+export function getAuthor(slug) {
+  return authorMap[slug];
+}
