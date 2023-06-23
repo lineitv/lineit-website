7e7b0428f49ca16e21e2488ec001f9c633a0043c
lineitv <134327909+lineitv@users.noreply.github.com>
2023-06-23 07:30:18 +0300

    Update Navbar.tsx

--- src/components/pages/Navbar.tsx
+++ src/components/pages/Navbar.tsx
@@ -162,9 +162,8 @@ export default function Header(en: any) {
                       </Disclosure.Button>
                       <Disclosure.Panel className="mt-2 space-y-2">
                         {[...products, ...callsToAction].map((item) => (
-                          <Link href={item.href}>
+                          <Link key={item.name} href={item.href}>
                             <Disclosure.Button
-                            key={item.name}
                             as="a"
                             className="block rounded-lg py-2 pl-6 pr-3 text-sm font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                                 >
